a56fd5823fce8ecfb80c6793d713ee6ad17c67db
gabriel <gabriel@studioplune.fr>
2024-03-06 19:13:46 +0100

    manifest, icons

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,20 +1,43 @@
-import { MetadataRoute } from 'next'
- 
+import { MetadataRoute } from "next";
+
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Gabriel Boyault | Développeur front-end',
-    short_name: 'Gabriel Boyault',
-    description: 'Portfolio de Gabriel Boyault, développeur front-end react & WordPress nantais',
-    start_url: '/',
-    display: 'standalone',
-    background_color: '#242327',
-    theme_color: '#EEEEEE',
-    icons: [
+    name: "Gabriel Boyault | Développeur front-end",
+    short_name: "Gabriel Boyault",
+    description:
+      "Portfolio de Gabriel Boyault, développeur front-end react & WordPress nantais",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#242327",
+    theme_color: "#EEEEEE",
+    screenshots: [
       {
-        src: '/favicon.ico',
-        sizes: 'any',
-        type: 'image/x-icon',
+        src: "/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
       },
     ],
-  }
+    icons: [
+      {
+        src: "/icon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
+        src: "/icon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+      {
+        src: "/icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
 }
