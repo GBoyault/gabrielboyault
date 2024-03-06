3973e37ac34299d6c125383e9297aa1d3e6b2a4c
gabriel <gabriel@studioplune.fr>
2024-03-06 20:07:24 +0100

    opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,25 @@ import "./globals.css";
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Gabriel Boyault | Développeur front-end",
+  title: "Gabriel Boyault | Développeur react / WordPress",
   description:
     "Bienvenue sur le portfolio de Gabriel Boyault, développeur front-end react & WordPress nantais",
+  openGraph: {
+    title: "Gabriel Boyault | Développeur react / WordPress",
+    description:
+      "Bienvenue sur le portfolio de Gabriel Boyault, développeur front-end react & WordPress nantais",
+    url: "https://www.gabrielboyault.com/",
+    siteName: "Gabriel Boyault | Développeur react / WordPress",
+    images: [
+      {
+        url: "/opengraph-image.jpg",
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: "fr_FR",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
@@ -17,7 +33,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
-      <body className={`${montserrat.className} text-sm/6 md:text-base/6 text-white-light bg-background`}>{children}</body>
+      <body
+        className={`${montserrat.className} text-sm/6 md:text-base/6 text-white-light bg-background`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
