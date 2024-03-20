ed4014d4b977fa5d74b548b71488efd30d6f16f2
gabriel <gabriel@studioplune.fr>
2024-03-20 18:18:24 +0100

    content: added project cottageparks

--- app/data/projects/cottageparks.tsx
+++ app/data/projects/cottageparks.tsx
@@ -5,17 +5,38 @@ const cottageparks: Project = {
   description: (
     <>
       <p className="mb-3">
-        Sur le site WordPress d'un groupe d'hôtellerie de plein air,
-        développement des fonctionnalités de réservation : recherche de
-        disponibilités et tarifs en temps réel, filtrage de locatifs, carte
-        dynamique. Développement de blocs Gutenberg sur-mesure.
+        Développement des fonctionnalités de réservation sur le site WordPress
+        d'un groupe d'hôtellerie de plein air : recherche de disponibilités et
+        tarifs en temps réel, filtrage de locatifs, carte dynamique.
+        Développement de blocs Gutenberg sur-mesure.
       </p>
-      <p>Travail au sein de l'agence Studio Plune.</p>
+      <p>Travail réalisé au sein de l'agence Studio Plune.</p>
     </>
   ),
   links: [{ title: "Voir le site", url: "https://www.cottageparks.fr/" }],
-  technos: ["WordPress", "React", "TypeScript", "Gutenberg", "leaflet"],
-  images: [],
+  technos: ["React", "TypeScript", "Zod", "Leaflet", "WordPress", "Gutenberg"],
+  images: [
+    {
+      src: "/cottageparks/main.png",
+      alt: "Aperçu de Cottage Parks",
+    },
+    {
+      src: "/cottageparks/map-desktop.webp",
+      alt: "Proposition de campings par rapport aux critères utilisateur et aux disponibilités",
+    },
+    {
+      src: "/cottageparks/filters-desktop.webp",
+      alt: "Filtrage des locatifs",
+    },
+    {
+      src: "/cottageparks/product-desktop.webp",
+      alt: "Fiche produit avec données de réservation dynamiques",
+    },
+    {
+      src: "/cottageparks/alternatives-desktop.webp",
+      alt: "Contre-propositions en cas de séjour indisponible",
+    },
+  ],
 };
 
 export default cottageparks;
